dd57cccef822662d964db0ed7d93722d1cad28ea
JarnoRFB <ruedigerbusche@web.de>
2019-01-06 22:46:10 +0100

    Hardcode dbname

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,5 +17,5 @@ def loader():
         mongo_uri = f'mongodb://{os.environ["MONGO_INITDB_ROOT_USERNAME"]}:{os.environ["MONGO_INITDB_ROOT_PASSWORD"]}@localhost:27017/?authMechanism=SCRAM-SHA-1'
     loader = ExperimentLoader(
         mongo_uri=mongo_uri,
-        db_name=os.environ['MONGO_DATABASE'])
+        db_name='incense_test')
     return loader
