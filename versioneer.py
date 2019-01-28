a4dc5cc284d8c0b34abaf90cad563b7bce269e66
JarnoRFB <ruedigerbusche@web.de>
2019-01-28 12:09:58 +0100

    Add codecov dependency

--- versioneer.py
+++ versioneer.py
@@ -1569,7 +1569,7 @@ def get_cmdclass():
         #   ...
 
         class cmd_build_exe(_build_exe):
-            def run(self):
+            def run(self):  # lgtm [py/similar-function]
                 root = get_root()
                 cfg = get_config_from_root(root)
                 versions = get_versions()
@@ -1598,7 +1598,7 @@ def get_cmdclass():
             from py2exe.build_exe import py2exe as _py2exe  # py2
 
         class cmd_py2exe(_py2exe):
-            def run(self):
+            def run(self):  # lgtm [py/similar-function]
                 root = get_root()
                 cfg = get_config_from_root(root)
                 versions = get_versions()
