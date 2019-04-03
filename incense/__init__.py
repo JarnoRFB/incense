6651f62663f99412ca686eb0924632a4bc0042a1
JarnoRFB <ruedigerbusche@web.de>
2019-04-03 11:26:26 +0200

    Try isort again

--- incense/__init__.py
+++ incense/__init__.py
@@ -1,5 +1,5 @@
-from .experiment_loader import ExperimentLoader
 from ._version import get_versions
+from .experiment_loader import ExperimentLoader
 
 __version__ = get_versions()["version"]
 del get_versions
