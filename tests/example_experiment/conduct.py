9606eb0da8bee265377443d03799ae7abbd37a4c
JarnoRFB <ruedigerbusche@web.de>
2019-10-28 23:11:40 +0000

    Set tf seed manually

--- tests/example_experiment/conduct.py
+++ tests/example_experiment/conduct.py
@@ -98,6 +98,8 @@ def make_model():
 
 @ex.command
 def conduct(epochs, optimizer, _run):
+    tf.random.set_seed(42)
+
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
     x_train = x_train / 255.0
     x_test = x_test / 255.0
--- tests/test_projector.py
+++ tests/test_projector.py
@@ -32,7 +32,7 @@ def test_projection_with_aggregation(loader):
     expected_projected_data = {
         "exp_id": [1, 2, 3],
         "epochs": [1, 3, 1],
-        "training_loss_mean": [0.6407258245944977, 0.42567836346162685, 0.2221361954788367],
+        "training_loss_mean": [0.6407849373420079, 0.42588336391250287, 0.21881918181777],
     }
     expected_projected = pd.DataFrame(expected_projected_data).set_index("exp_id")
     projected = exps.project(on=["config.epochs", {"metrics.training_loss": np.mean}])
