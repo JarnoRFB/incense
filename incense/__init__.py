f53d8afbecbf5b1086ea188d8744587b95df8cd3
Rüdiger Busche <ruedigerbusche@web.de>
2020-05-12 20:11:49 +0200

    Add basic support for FileStorageObserver (#62)
    * Add basics for file system observer loader
    
    * Add env file for devcontainer
    
    * Add simple test

--- incense/__init__.py
+++ incense/__init__.py
@@ -1,5 +1,5 @@
 from ._version import get_versions
-from .experiment_loader import ExperimentLoader
+from .experiment_loader import ExperimentLoader, FileSystemExperimentLoader
 
 __version__ = get_versions()["version"]
 del get_versions
