0ac321f7010a47659fbde358b19bebac96d58d17
rbusche <rbusche@posteo.net>
2022-07-17 20:20:31 +0000

    Also remove local db in delete script

--- delete_db.py
+++ delete_db.py
@@ -1,14 +1,18 @@
 import os
+import shutil
 from pathlib import Path
 
 from dotenv import load_dotenv
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
 
 env_path = Path(".") / "infrastructure" / "sacred_setup" / ".env"
 load_dotenv(dotenv_path=env_path)
 
 DB_NAME = os.environ["MONGO_DATABASE"]
 
-client = MongoClient(host="mongo")
+client: MongoClient = MongoClient(host="mongo")
 client.drop_database(DB_NAME)
+
+shutil.rmtree(Path("~/data/incense_test/").expanduser(), ignore_errors=True)
+
 print(f"Database {DB_NAME} successfully deleted.")
